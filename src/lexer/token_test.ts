02a20fb9a481087e3e32ae26a483b1ecaab6719e
Sebastian Nunez <02snunez@gmail.com>
2025-01-03 12:02:35 -0500

    test: add tests for newToken

---
+++ src/lexer/token_test.ts
@@ -0,0 +1,23 @@
+import { describe, it } from "jsr:@std/testing/bdd";
+import { newToken, TokenType } from "./token.ts";
+import { assertEquals } from "@std/assert/equals";
+
+describe("newToken()", () => {
+  it("should create a token given empty value parameter", () => {
+    const value = "";
+    const type = TokenType.Equals;
+
+    const result = newToken(value, type);
+
+    assertEquals(result, { value, type });
+  });
+
+  it("should create a token given non-empty value", () => {
+    const value = "=";
+    const type = TokenType.Equals;
+
+    const result = newToken(value, type);
+
+    assertEquals(result, { value, type });
+  });
+});
